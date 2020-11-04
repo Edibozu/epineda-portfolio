36c25734952b908de6757b1c4e153f5b3c2450b7
Eduardo Pineda <edibozu@gmail.com>
2020-11-04 01:16:30 -0500

    sever is serving up client build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/epineda-portfolio", {
     useNewUrlParser: true,
@@ -31,6 +34,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
