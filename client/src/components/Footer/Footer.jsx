947e963770683420d7fa5cafa826b441b4b1a3bc
Eduardo Pineda <edibozu@gmail.com>
2020-11-11 10:59:08 -0500

    fixed footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <div className="card-footer text-muted text-center" id="footer">
+            <footer className="footer text-muted text-center" id="footer">
                 <p>Copyright <i className="far fa-copyright"> Eduardo Pineda 2020, All Rights Reserved.</i></p>
-            </div>
+            </footer>
         </div>
     );
 };
